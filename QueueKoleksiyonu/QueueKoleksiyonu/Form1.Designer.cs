4def955126a36177f683f61edeeae485fee14bc0
ErenTahaCagin <100272957+ErenTahaCagin@users.noreply.github.com>
2022-02-24 08:48:39 +0300

    Add files via upload

---
+++ QueueKoleksiyonu/QueueKoleksiyonu/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace QueueKoleksiyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSiraAl = new System.Windows.Forms.Button();
+            this.lbSiralar = new System.Windows.Forms.ListBox();
+            this.btnSiraCikart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSiraAl
+            // 
+            this.btnSiraAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSiraAl.Location = new System.Drawing.Point(12, 12);
+            this.btnSiraAl.Name = "btnSiraAl";
+            this.btnSiraAl.Size = new System.Drawing.Size(111, 68);
+            this.btnSiraAl.TabIndex = 0;
+            this.btnSiraAl.Text = "Sıra Al";
+            this.btnSiraAl.UseVisualStyleBackColor = true;
+            this.btnSiraAl.Click += new System.EventHandler(this.btnSiraAl_Click);
+            // 
+            // lbSiralar
+            // 
+            this.lbSiralar.FormattingEnabled = true;
+            this.lbSiralar.Location = new System.Drawing.Point(129, 12);
+            this.lbSiralar.Name = "lbSiralar";
+            this.lbSiralar.Size = new System.Drawing.Size(125, 173);
+            this.lbSiralar.TabIndex = 1;
+            // 
+            // btnSiraCikart
+            // 
+            this.btnSiraCikart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSiraCikart.Location = new System.Drawing.Point(260, 12);
+            this.btnSiraCikart.Name = "btnSiraCikart";
+            this.btnSiraCikart.Size = new System.Drawing.Size(111, 68);
+            this.btnSiraCikart.TabIndex = 2;
+            this.btnSiraCikart.Text = "Sıra Çıkart";
+            this.btnSiraCikart.UseVisualStyleBackColor = true;
+            this.btnSiraCikart.Click += new System.EventHandler(this.btnSiraCikart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 191);
+            this.Controls.Add(this.btnSiraCikart);
+            this.Controls.Add(this.lbSiralar);
+            this.Controls.Add(this.btnSiraAl);
+            this.Name = "Form1";
+            this.Text = "Queue Uygulaması";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSiraAl;
+        private System.Windows.Forms.ListBox lbSiralar;
+        private System.Windows.Forms.Button btnSiraCikart;
+    }
+}
+
---
+++ QueueKoleksiyonu/QueueKoleksiyonu/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QueueKoleksiyonu
+{
+    public partial class Form1 : Form
+    {
+        private Queue kuyruk = new Queue();
+        private int sira = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSiraAl_Click(object sender, EventArgs e)
+        {
+            sira++;
+            kuyruk.Enqueue(sira);
+            Listele();
+        }
+
+        private void Listele()
+        {
+            lbSiralar.Items.Clear();
+            foreach (int sira in kuyruk)
+            {
+                lbSiralar.Items.Add(sira);
+            }
+        }
+
+        private void btnSiraCikart_Click(object sender, EventArgs e)
+        {
+            kuyruk.Dequeue();
+            Listele();
+        }
+    }
+}
